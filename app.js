5504acf57802f9a58aba6fe2c23a411886ba4b09
Kail Fu <Kailiang_fu@brown.edu>
2022-09-10 17:40:44 -0400

    yuan scholar

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -21,7 +21,7 @@ const apiRequest = async () => {
 
   // Making a fetch request to an API endpoint
   // Note: a fetch request is an asynchronous operation, and `await` tells the program to wait until the request has been completed before continuing
-  const response = await fetch(baseUrl + endpoint, { 
+  const response = await fetch(baseUrl + endpoint, {
     method: "GET",
     headers: {
       'Content-Type': 'application/json',
@@ -44,10 +44,23 @@ const updatePage = async () => {
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
+  const filteredArray = fruitsArray.filter((item) => {
+    return item.nutritions.sugar > 15;
+  })
+
+  const nameArray = filteredArray.map((item) => {
+    return item.name;
+  })
+
+  const kail_scholar = nameArray.join('-')
+  console.log(kail_scholar)
 
   // TODO: Create a new HTML element to display your data 
+  const newElement = document.createElement('div');
+  newElement.innerHTML = kail_scholar;
 
   // TODO: Append your new element to the page
+  gallery.append(newElement);
 
 }
 
